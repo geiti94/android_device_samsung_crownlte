e8afaa2f098b632f13fb667ed856c80cebc09f8c
Christopher A. Williamson <home@chrisaw.com>
2019-01-12 11:21:55 +0000

    fixed dtbTool name

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -21,7 +21,7 @@ include device/qcom/common/dtbtool/Android.mk
 endif
 
 ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
-DTBTOOL_NAME := dtbToolExynos
+DTBTOOL_NAME := dtbToolCM
 else
 DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
 endif
