e450b818c72c8e1d25c86f5e93f797a4f6f1bec3
Christopher A. Williamson <home@chrisaw.com>
2019-01-14 09:27:31 +0000

    switched fallback from dtbToolLineage to dtbToolCM - more common across recoveries

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -21,7 +21,7 @@ include device/qcom/common/dtbtool/Android.mk
 endif
 
 ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
-DTBTOOL_NAME := dtbToolLineage
+DTBTOOL_NAME := dtbToolCM
 else
 DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
 endif
